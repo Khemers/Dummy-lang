fa2a7dc487a7abb1a893a90325ee922ca228834a
Bernardo Facchi <bernardobf2010@hotmail.com>
2021-01-10 21:44:31 -0300

    Scanner

--- fonte/Main.java
+++ fonte/Main.java
@@ -1,6 +1,19 @@
-public class Main {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.util.Scanner;
+import java.nio.file.Paths;
 
-        String fileName = args[0];
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        Scanner file = new Scanner(Paths.get(args[0]));
+        StringBuilder code = new StringBuilder();
+
+        while (file.hasNextLine()) {
+            code.append(file.nextLine());
+            code.append("\n");
+        }
+
+        System.out.println(code);
+
     }
 }
