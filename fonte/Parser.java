7478803aa4bd661a027795664734c4ef6365b89d
vinibmittmann <vinybmittmann@hotmail.com>
2021-01-10 22:30:34 -0300

    Parser

--- fonte/Parser.java
+++ fonte/Parser.java
@@ -1,11 +1,15 @@
-import java.util.regex.Pattern;
-import java.util.regex.Matcher;
+//import java.util.regex.Pattern;
+//import java.util.regex.Matcher;
 
 public class Parser {
-    
+
+    public String codigoFonte;
+ 
     public Parser(String codigoFonte) {
         setCodigoFonte(codigoFonte);
+    }
 
     public void setCodigoFonte(String codigoFonte) {
         this.codigoFonte = codigoFonte;
     }
+}
