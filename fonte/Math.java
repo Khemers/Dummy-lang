3056554cef7a23105eda7dfaa198d90c7328c87f
leonardo machado <mestreleozinhoocara@gmail.com>
2021-01-15 23:20:17 -0300

    ifa

--- fonte/Math.java
+++ fonte/Math.java
@@ -4,30 +4,82 @@ public class Math {
         if (var.type.equals("int")) {
             var.setValue(String.valueOf(Integer.parseInt(a) + Integer.parseInt(b)));
         }
+        else{
+            if (var.type.equals("float")) {
+                var.setValue(String.valueOf(Float.parseFloat(a) + Float.parseFloat(b)));}
+            
+            if (var.type.equals("double")) {
+                var.setValue(String.valueOf(Double.parseDouble(a) + Double.parseDouble(b)));}
+
+            if (var.type.equals("string")) {
+                 var.setValue(String.valueOf(a + b));}
+            }      
+           
     }
 
     public static void sub(Variable var, String a, String b) {
         if (var.type.equals("int")) {
             var.setValue(String.valueOf(Integer.parseInt(a) - Integer.parseInt(b)));
         }
+        else{
+            if (var.type.equals("float")) {
+                var.setValue(String.valueOf(Float.parseFloat(a) + Float.parseFloat(b)));}
+            
+            if (var.type.equals("double")) {
+                var.setValue(String.valueOf(Double.parseDouble(a) + Double.parseDouble(b)));}
+                
+            if (var.type.equals("string")) {
+                 var.setValue(String.valueOf(a + b));}
+            } 
+    
     }
 
     public static void mult(Variable var, String a, String b) {
         if (var.type.equals("int")) {
             var.setValue(String.valueOf(Integer.parseInt(a) * Integer.parseInt(b)));
         }
+        else{
+            if (var.type.equals("float")) {
+                var.setValue(String.valueOf(Float.parseFloat(a) + Float.parseFloat(b)));}
+            
+            if (var.type.equals("double")) {
+                var.setValue(String.valueOf(Double.parseDouble(a) + Double.parseDouble(b)));}
+                
+            if (var.type.equals("string")) {
+                 var.setValue(String.valueOf(a + b));}
+            } 
 
     }
     public static void div(Variable var, String a, String b) {
         if (var.type.equals("int")) {
             var.setValue(String.valueOf(Integer.parseInt(a) / Integer.parseInt(b)));
         }
+        else{
+            if (var.type.equals("float")) {
+                var.setValue(String.valueOf(Float.parseFloat(a) + Float.parseFloat(b)));}
+            
+            if (var.type.equals("double")) {
+                var.setValue(String.valueOf(Double.parseDouble(a) + Double.parseDouble(b)));}
+                
+            if (var.type.equals("string")) {
+                 var.setValue(String.valueOf(a + b));}
+            } 
 
     }
     public static void rest(Variable var, String a, String b) {
         if (var.type.equals("int")) {
             var.setValue(String.valueOf(Integer.parseInt(a) % Integer.parseInt(b)));
         }
+        else{
+            if (var.type.equals("float")) {
+                var.setValue(String.valueOf(Float.parseFloat(a) + Float.parseFloat(b)));}
+            
+            if (var.type.equals("double")) {
+                var.setValue(String.valueOf(Double.parseDouble(a) + Double.parseDouble(b)));}
+                
+            if (var.type.equals("string")) {
+                 var.setValue(String.valueOf(a + b));}
+            } 
 
     }
     public boolean comparaIgual(Variable var, String a, String b) {
